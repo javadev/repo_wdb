f67067244c688d2f5d4965f005d5d93a09c9c672
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-07 15:11:20 +0300

    Create DefaultLineEndRenderer.java

---
+++ wdb-api/src/main/java/org/wdbuilder/view/line/end/DefaultLineEndRenderer.java
@@ -0,0 +1,13 @@
+package org.wdbuilder.view.line.end;
+
+import org.wdbuilder.view.ILineEndRenderer;
+
+public class DefaultLineEndRenderer implements ILineEndRenderer {
+
+  @Override
+	public void draw(ILineEndRenderer renderCtx) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
