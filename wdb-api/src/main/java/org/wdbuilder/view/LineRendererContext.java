90cbc8806dabd8d206077da1e646311f3531c88a
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:44:22 +0300

    Create LineRendererContext.java

---
+++ wdb-api/src/main/java/org/wdbuilder/view/LineRendererContext.java
@@ -0,0 +1,29 @@
+package org.wdbuilder.view;
+
+import org.wdbuilder.domain.helper.Point;
+
+class LineRendererContext extends LineRendererContextBase implements
+  	ILineRendererContext {
+
+	private Point begin;
+	private Point end;
+
+	@Override
+	public Point getBegin() {
+		return begin;
+	}
+
+	@Override
+	public Point getEnd() {
+		return end;
+	}
+
+	void setBegin(Point begin) {
+		this.begin = begin;
+	}
+
+	void setEnd(Point end) {
+		this.end = end;
+	}
+
+}
