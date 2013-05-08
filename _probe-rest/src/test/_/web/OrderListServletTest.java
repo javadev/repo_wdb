d97f0e7b568cc108e6a580ddb75747fd3b5f24a9
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 19:55:56 +0300

    Update OrderListServletTest.java

--- _probe-rest/src/test/_/web/OrderListServletTest.java
+++ _probe-rest/src/test/_/web/OrderListServletTest.java
@@ -49,6 +49,31 @@ public class OrderListServletTest {
 		assertEquals("{\"orders\":[]}", out.toString());
 	}	
 	
+	@Test
+	public void testMe() throws ServletException, IOException {
+		HttpServletRequest request = mock( HttpServletRequest.class);
+		when( request.getParameter("c")).thenReturn("me");
+		
+		HttpServletResponse response = mock( HttpServletResponse.class);
+		StringWriter out = new StringWriter();
+		when( response.getWriter()).thenReturn( new PrintWriter( new PrintWriter(out) ));
+				
+		servlet.doGet(request, response);
+		
+		out.close();		
+		assertEquals("{\"orders\":[" +
+				"{\"id\":\"id-1\",\"consumer\":\"me\"," +
+				"\"positions\":[" +
+				"{\"goodId\":\"BREAD\",\"quantity\":4,\"price\":0.67}," +
+				"{\"goodId\":\"BUTTER\",\"quantity\":2,\"price\":7.0}" +
+				"]}," +
+				"{\"id\":\"id-3\",\"consumer\":\"me\"," +
+				"\"positions\":[" +
+				"{\"goodId\":\"BIKE\",\"quantity\":4,\"price\":120.0}" +
+				"]}" +
+				"]}", out.toString());
+	}		
+	
 	@Before
 	public void setUp() throws ServletException {
 		servlet.init();
