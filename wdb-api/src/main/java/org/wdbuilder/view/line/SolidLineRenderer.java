5e8066263d867c5dd2bb966674bf97814378f69b
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:47:08 +0300

    Update SolidLineRenderer.java

--- wdb-api/src/main/java/org/wdbuilder/view/line/SolidLineRenderer.java
+++ wdb-api/src/main/java/org/wdbuilder/view/line/SolidLineRenderer.java
@@ -5,12 +5,15 @@ import org.wdbuilder.view.ILineRendererContext;
 
 public class SolidLineRenderer implements ILineRenderer<ILineRendererContext> {
 
-  @Override
+	@Override
 	public void draw(ILineRendererContext renderCtx) {
-		// TODO Auto-generated method stub
-		
-	}
+		int x0 = renderCtx.getBegin().getX();
+		int y0 = renderCtx.getBegin().getY();
+		int x1 = renderCtx.getEnd().getX();
+		int y1 = renderCtx.getEnd().getY();
 
-	
+		renderCtx.getGraphics().setColor(renderCtx.getColor());
+		renderCtx.getGraphics().drawLine(x0, y0, x1, y1);
+	}
 
 }
