ec2d5b952172cd2f66accb56cae78b45bad96a75
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:33:38 +0300

    Update ILineRenderer.java

--- wdb-api/src/main/java/org/wdbuilder/view/ILineRenderer.java
+++ wdb-api/src/main/java/org/wdbuilder/view/ILineRenderer.java
@@ -1,6 +1,5 @@
 package org.wdbuilder.view;
 
-public interface ILineRenderer<T extends ILineRendererContext> {
+public interface ILineRenderer<T extends ILineRendererContextBase> {
   public void draw( T renderCtx);
 }
-
