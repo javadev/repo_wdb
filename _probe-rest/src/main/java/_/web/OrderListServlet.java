a534d14e5818b54b9017833cf31a4fcb6577320e
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 18:15:12 +0300

    Create OrderListServlet.java

---
+++ _probe-rest/src/main/java/_/web/OrderListServlet.java
@@ -0,0 +1,10 @@
+package _.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@SuppressWarnings("serial")
+@WebServlet("/order-list")
+public class OrderListServlet extends HttpServlet {
+
+}
