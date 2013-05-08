4272061d0cfd36efdd9e8eeba6dc35d408e2c457
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:20:00 +0300

    Update ILineEndRendererContext.java

--- wdb-api/src/main/java/org/wdbuilder/view/ILineEndRendererContext.java
+++ wdb-api/src/main/java/org/wdbuilder/view/ILineEndRendererContext.java
@@ -3,7 +3,7 @@ package org.wdbuilder.view;
 import org.wdbuilder.domain.LinkSocket;
 import org.wdbuilder.domain.helper.Point;
 
-interface ILineEndRendererContext extends ILineRendererContext {
+public interface ILineEndRendererContext extends ILineRendererContext {
 
 	Point getBaseLocation();
 
