6382f304b4d0f7df6c0de30d65635174be9bd1e3
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 18:55:02 +0300

    Update FileOrderService.java

--- _probe-rest/src/main/java/_/service/FileOrderService.java
+++ _probe-rest/src/main/java/_/service/FileOrderService.java
@@ -1,23 +1,25 @@
 package _.service;
 
+import java.io.File;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
 
 import _.domain.OrderList;
 
 public class FileOrderService implements IOrderService {
-	
+
 	private static final String FILE_NAME = "order-db.xml";
 
 	@Override
 	public OrderList retrieveForConsumer(String consumer) throws Exception {
 		JAXBContext context = JAXBContext.newInstance(OrderList.class);
 		Unmarshaller unmarshaller = context.createUnmarshaller();
-		
-		
-		
-		// TODO Auto-generated method stub
-		return null;
+
+		Object obj = unmarshaller.unmarshal(new File(FILE_NAME));
+		OrderList result = OrderList.class.cast(obj);
+
+		return result;
 	}
 
 }
