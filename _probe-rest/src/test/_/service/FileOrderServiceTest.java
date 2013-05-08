42d32fa3756b4ddcaabd444534bd6b6608777c14
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 18:35:05 +0300

    Create FileOrderServiceTest.java

---
+++ _probe-rest/src/test/_/service/FileOrderServiceTest.java
@@ -0,0 +1,143 @@
+package _.service;
+
+import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
+import java.util.Iterator;
+
+import org.junit.Test;
+import _.domain.Order;
+
+import static org.junit.Assert.*;
+
+public class FileOrderServiceTest {
+
+  private IOrderService service = new FileOrderService();
+
+	@Test
+	public void testGetOrderListForNull() {
+		Collection<Order> collection = service.retrieveForConsumer(null);
+		assertNotNull(collection);
+		assertTrue(collection.isEmpty());
+	}
+
+	@Test
+	public void testGetOrderListForAbsentConsumer() {
+		Collection<Order> collection = service.retrieveForConsumer("nobody");
+		assertNotNull(collection);
+		assertTrue(collection.isEmpty());
+	}
+
+	@Test
+	public void testGetOrderListForExistingConsumer() {
+		Collection<Order> collection = service.retrieveForConsumer("me");
+		assertNotNull(collection);
+		assertEquals(2, collection.size());
+		Iterator<Order> it = collection.iterator();
+		assertOrder("id-1", getDate("14/04/2013"), "me", it.next(),
+				new ExpectedPosition() {
+
+					@Override
+					public int getExpectedCount() {
+						return 2;
+					}
+
+					@Override
+					public String getGoodId(int num) {
+						switch (num) {
+						case 0:
+							return "BREAD";
+						case 1:
+							return "BUTTER";
+						}
+						return null;
+					}
+
+					@Override
+					public BigDecimal getPrice(int num) {
+						switch (num) {
+						case 0:
+							return new BigDecimal(4.0);
+						case 1:
+							return new BigDecimal(7.0);
+						}
+						return null;
+					}
+
+					@Override
+					public int getQuantity(int num) {
+						switch (num) {
+						case 0:
+							return 4;
+						case 1:
+							return 2;
+						}
+						return 0;
+					}
+
+				});
+		assertOrder("id-3", getDate("25/04/2013"), "me", it.next(),
+				new ExpectedPosition() {
+
+					@Override
+					public int getExpectedCount() {
+						return 1;
+					}
+
+					@Override
+					public String getGoodId(int num) {
+						switch (num) {
+						case 0:
+							return "BIKE";
+						}
+						return null;
+					}
+
+					@Override
+					public BigDecimal getPrice(int num) {
+						switch (num) {
+						case 0:
+							return new BigDecimal(120.0);
+						}
+						return null;
+					}
+
+					@Override
+					public int getQuantity(int num) {
+						switch (num) {
+						case 0:
+							return 4;
+						}
+						return 0;
+					}
+				});
+	}
+
+	private void assertOrder(String expectedId, Date expectedDate,
+			String expectedConsumer, Order order,
+			ExpectedPosition expectedPosition) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private interface ExpectedPosition {
+		int getExpectedCount();
+
+		String getGoodId(int num);
+
+		BigDecimal getPrice(int num);
+
+		int getQuantity(int num);
+	}
+
+	private static final Date getDate(String str) {
+		try {
+			return new SimpleDateFormat("dd/MM/yyyy").parse(str);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return null;
+		}
+	}
+
+}
