b32e2cc9ef094f470df0d3a73c2b88aaaa6d47d6
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:31:34 +0300

    Create SolidLineRenderer.java

---
+++ wdb-api/src/main/java/org/wdbuilder/view/line/SolidLineRenderer.java
@@ -0,0 +1,16 @@
+package org.wdbuilder.view.line;
+
+import org.wdbuilder.view.ILineRenderer;
+import org.wdbuilder.view.ILineRendererContext;
+
+public class SolidLineRenderer implements ILineRenderer<ILineRendererContext> {
+
+  @Override
+	public void draw(ILineRendererContext renderCtx) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
