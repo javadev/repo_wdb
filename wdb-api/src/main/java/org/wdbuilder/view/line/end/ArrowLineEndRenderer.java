55988fc3ae4ff3198db89a9cb955790c7b9611c5
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 11:23:44 +0300

    Update ArrowLineEndRenderer.java

--- wdb-api/src/main/java/org/wdbuilder/view/line/end/ArrowLineEndRenderer.java
+++ wdb-api/src/main/java/org/wdbuilder/view/line/end/ArrowLineEndRenderer.java
@@ -1,5 +1,7 @@
 package org.wdbuilder.view.line.end;
 
+import java.awt.Graphics2D;
+
 import org.wdbuilder.domain.helper.Point;
 import org.wdbuilder.view.ILineEndRenderer;
 import org.wdbuilder.view.ILineEndRendererContext;
@@ -8,47 +10,81 @@ import static org.wdbuilder.service.DiagramService.LINE_OFFSET;
 
 public class ArrowLineEndRenderer implements ILineEndRenderer {
 
-  @Override
+	@Override
 	public void draw(ILineEndRendererContext renderCtx) {
-		renderCtx.getGraphics().setColor(renderCtx.getColor());
-		int[] x;
-		int[] y;
-		Point base = renderCtx.getBaseLocation();
-		int px = base.getX();
-		int py = base.getY();
-		Point p0, p1;
+		Graphics2D gr = renderCtx.getGraphics();
+		gr.setColor(renderCtx.getColor());
+		Point p0 = getSmallLineStart(renderCtx);
+		Point p1 = getSmallLineEnd(renderCtx);
+		int[] x = getX(renderCtx);
+		int[] y = getY(renderCtx);
 
+		gr.drawLine(p0.getX(), p0.getY(), p1.getX(), p1.getY());
+		gr.fillPolygon(x, y, 3);
+	}
+
+	private static Point getSmallLineStart(ILineEndRendererContext renderCtx) {
+		Point base = renderCtx.getBaseLocation();
 		switch (renderCtx.getDirection()) {
 		case LEFT:
-			p0 = base.addX(-LINE_OFFSET);
-			p1 = p0.addX(7);
-			x = new int[] { px, px - 7, px - 7 };
-			y = new int[] { py, py - 3, py + 4 };
-			break;
+			return base.addX(-LINE_OFFSET);
 		case RIGHT:
-			p0 = base.addX(LINE_OFFSET);
-			p1 = p0.addX(-7);
-			x = new int[] { px, px + 7, px + 7 };
-			y = new int[] { py, py - 3, py + 4 };
-			break;
+			return base.addX(LINE_OFFSET);
 		case TOP:
-			p0 = base.addY(-LINE_OFFSET);
-			p1 = p0.addY(7);
-			x = new int[] { px, px - 3, px + 4 };
-			y = new int[] { py, py - 7, py - 7 };
-			break;
+			return base.addY(-LINE_OFFSET);
 		case BOTTOM:
-			p0 = base.addY(LINE_OFFSET);
-			p1 = p0.addY(-7);
-			x = new int[] { px, px - 3, px + 4 };
-			y = new int[] { py, py + 7, py + 7 };
-			break;
+			return base.addY(LINE_OFFSET);
 		default:
 			throw new IllegalArgumentException("Link socket direction is null");
 		}
-		renderCtx.getGraphics().drawLine(p0.getX(), p0.getY(), p1.getX(),
-				p1.getY());
-		renderCtx.getGraphics().fillPolygon(x, y, 3);
+	}
+
+	private static Point getSmallLineEnd(ILineEndRendererContext renderCtx) {
+		Point p0 = getSmallLineStart(renderCtx);
+		switch (renderCtx.getDirection()) {
+		case LEFT:
+			return p0.addX(7);
+		case RIGHT:
+			return p0.addX(-7);
+		case TOP:
+			return p0.addY(-7);
+		case BOTTOM:
+			return p0.addY(7);
+		default:
+			throw new IllegalArgumentException("Link socket direction is null");
+		}
+	}
+
+	private static int[] getX(ILineEndRendererContext renderCtx) {
+		Point base = renderCtx.getBaseLocation();
+		int x = base.getX();
+		switch (renderCtx.getDirection()) {
+		case LEFT:
+			return new int[] { x, x - 7, x - 7 };
+		case RIGHT:
+			return new int[] { x, x + 7, x + 7 };
+		case TOP:
+		case BOTTOM:
+			return new int[] { x, x - 3, x + 4 };
+		default:
+			throw new IllegalArgumentException("Link socket direction is null");
+		}
+	}
+
+	private static int[] getY(ILineEndRendererContext renderCtx) {
+		Point base = renderCtx.getBaseLocation();
+		int y = base.getX();
+		switch (renderCtx.getDirection()) {
+		case LEFT:
+		case RIGHT:
+			return new int[] { y, y - 3, y + 4 };
+		case TOP:
+			return new int[] { y, y - 7, y - 7 };
+		case BOTTOM:
+			return new int[] { y, y + 7, y + 7 };
+		default:
+			throw new IllegalArgumentException("Link socket direction is null");
+		}
 	}
 
 }
