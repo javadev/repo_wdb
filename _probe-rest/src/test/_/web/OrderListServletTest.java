0f81a9a22e233cbcd228372e6e4b6413c3c0cea9
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 19:49:59 +0300

    Update OrderListServletTest.java

--- _probe-rest/src/test/_/web/OrderListServletTest.java
+++ _probe-rest/src/test/_/web/OrderListServletTest.java
@@ -1,7 +1,12 @@
 package _.web;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -13,14 +18,20 @@ import org.junit.Test;
 
 public class OrderListServletTest {
 	
-	private OrderListServlet servlet = new OrderListServlet();
+	private OrderListServlet servlet= new OrderListServlet();
 
 	@Test
-	public void testNoParameter() throws ServletException {
+	public void testNoParameter() throws ServletException, IOException {
 		HttpServletRequest request = mock( HttpServletRequest.class);
 		HttpServletResponse response = mock( HttpServletResponse.class);
-		
+
+		StringWriter out = new StringWriter();
+		when( response.getWriter()).thenReturn( new PrintWriter( new PrintWriter(out) ));
+				
 		servlet.doGet(request, response);
+		
+		out.close();		
+		assertEquals("{\"orders\":[]}", out.toString());
 	}
 	
 	@Before
