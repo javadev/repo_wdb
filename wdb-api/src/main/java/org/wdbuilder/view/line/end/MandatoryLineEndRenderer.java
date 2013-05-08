8dd5e3b606d01b397332b98f38367a419d64ef6d
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-08 14:21:04 +0300

    Update MandatoryLineEndRenderer.java

--- wdb-api/src/main/java/org/wdbuilder/view/line/end/MandatoryLineEndRenderer.java
+++ wdb-api/src/main/java/org/wdbuilder/view/line/end/MandatoryLineEndRenderer.java
@@ -3,14 +3,15 @@ package org.wdbuilder.view.line.end;
 import java.awt.Graphics2D;
 
 import org.wdbuilder.domain.helper.Point;
-import org.wdbuilder.view.ILineEndRenderer;
+import org.wdbuilder.view.ILineRenderer;
 import org.wdbuilder.view.ILineEndRendererContext;
 
 import static org.wdbuilder.service.DiagramService.LINE_OFFSET;
 
-public abstract class MandatoryLineEndRenderer implements ILineEndRenderer {
-	
-	protected abstract void drawCrossingAngle( ILineEndRendererContext renderCtx );
+public abstract class MandatoryLineEndRenderer implements
+		ILineRenderer<ILineEndRendererContext> {
+
+	protected abstract void drawCrossingAngle(ILineEndRendererContext renderCtx);
 
 	@Override
 	public void draw(ILineEndRendererContext renderCtx) {
@@ -45,44 +46,44 @@ public abstract class MandatoryLineEndRenderer implements ILineEndRenderer {
 			throw new IllegalArgumentException("Link socket direction is null");
 		}
 	}
-	
+
 	private static Point[] getAcross(ILineEndRendererContext renderCtx) {
 		Point[] result = new Point[2];
 		Point p = renderCtx.getBaseLocation();
 		switch (renderCtx.getDirection()) {
 		case LEFT:
-			result[0] = new Point( p.getX() - 7, p.getY() - 5 );
-			result[1] = new Point( p.getX() - 7, p.getY() + 5 );
+			result[0] = new Point(p.getX() - 7, p.getY() - 5);
+			result[1] = new Point(p.getX() - 7, p.getY() + 5);
 			break;
 		case RIGHT:
-			result[0] = new Point( p.getX() + 7, p.getY() - 5 );
-			result[1] = new Point( p.getX() + 7, p.getY() + 5 );
+			result[0] = new Point(p.getX() + 7, p.getY() - 5);
+			result[1] = new Point(p.getX() + 7, p.getY() + 5);
 			break;
 		case TOP:
-			result[0] = new Point( p.getX() - 5, p.getY() - 7 );
-			result[1] = new Point( p.getX() + 5, p.getY() - 7 );
+			result[0] = new Point(p.getX() - 5, p.getY() - 7);
+			result[1] = new Point(p.getX() + 5, p.getY() - 7);
 			break;
 		case BOTTOM:
-			result[0] = new Point( p.getX() - 5, p.getY() + 7 );
-			result[1] = new Point( p.getX() + 5, p.getY() + 7 );
+			result[0] = new Point(p.getX() - 5, p.getY() + 7);
+			result[1] = new Point(p.getX() + 5, p.getY() + 7);
 			break;
 		default:
 			throw new IllegalArgumentException("Link socket direction is null");
 		}
 		return result;
 	}
-	
+
 	public static class One extends MandatoryLineEndRenderer {
 
 		@Override
 		protected void drawCrossingAngle(ILineEndRendererContext renderCtx) {
 			// Do nothing
 		}
-		
+
 	}
-	
+
 	private static final int[] STEPS = { -5, 5 };
-	
+
 	public static class Many extends MandatoryLineEndRenderer {
 
 		@Override
@@ -93,21 +94,22 @@ public abstract class MandatoryLineEndRenderer implements ILineEndRenderer {
 			switch (renderCtx.getDirection()) {
 			case LEFT:
 			case RIGHT:
-				for( int n : STEPS ) {
-					gr.drawLine(p.getX(), p.getY(), b.getX(), b.getY()+ n );					
+				for (int n : STEPS) {
+					gr.drawLine(p.getX(), p.getY(), b.getX(), b.getY() + n);
 				}
 				break;
 			case TOP:
 			case BOTTOM:
-				for( int n : STEPS ) {
-					gr.drawLine(p.getX(), p.getY(), b.getX() + n, b.getY() );					
+				for (int n : STEPS) {
+					gr.drawLine(p.getX(), p.getY(), b.getX() + n, b.getY());
 				}
 				break;
 			default:
-				throw new IllegalArgumentException("Link socket direction is null");
+				throw new IllegalArgumentException(
+						"Link socket direction is null");
 			}
 		}
-		
+
 	}
-	
+
 }
