0fca59910adae1457c7f81c8a62e3f16f60d5b67
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-09 13:53:37 +0300

    moving on (2)

--- wdb-api/src/main/java/org/wdbuilder/view/line/LineEnd.java
+++ wdb-api/src/main/java/org/wdbuilder/view/line/LineEnd.java
@@ -1,19 +1,28 @@
 package org.wdbuilder.view.line;
 
-import org.wdbuilder.view.ILineEndRenderer;
+import org.wdbuilder.view.ILineEndRendererContext;
+import org.wdbuilder.view.ILineRenderer;
+import org.wdbuilder.view.line.end.ArrowLineEndRenderer;
+import org.wdbuilder.view.line.end.DefaultLineEndRenderer;
 import org.wdbuilder.view.line.end.ILineEnd;
 
 public enum LineEnd implements ILineEnd {
   SIMPLE( "simple" ) {
 		@Override
-		public ILineEndRenderer getRenderer() {
+		public ILineRenderer<ILineEndRendererContext> getRenderer() {
 			return new DefaultLineEndRenderer();
 		}		
 	},
 	SOLID_ARROW( "solid arrow" ) {
 		@Override
-		public ILineEndRenderer getRenderer() {
-			return new ArrowLineEndRenderer();
+		public ILineRenderer<ILineEndRendererContext> getRenderer() {
+			return new ArrowLineEndRenderer.Solid();
+		}		
+	}, 	
+	TRANSPARENT_ARROW( "transparent arrow" ) {
+		@Override
+		public ILineRenderer<ILineEndRendererContext> getRenderer() {
+			return new ArrowLineEndRenderer.Transparent();
 		}		
 	}, 	
 	;
