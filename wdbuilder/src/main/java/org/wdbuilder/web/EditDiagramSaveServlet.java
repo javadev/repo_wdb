ee6d7595f3ae7d980d3387743dad57d1a0e607ec
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-27 18:51:32 +0300

    Update EditDiagramSaveServlet.java

--- wdbuilder/src/main/java/org/wdbuilder/web/EditDiagramSaveServlet.java
+++ wdbuilder/src/main/java/org/wdbuilder/web/EditDiagramSaveServlet.java
@@ -5,9 +5,9 @@ import javax.servlet.annotation.WebServlet;
 import org.wdbuilder.input.BlockParameter;
 import org.wdbuilder.web.base.ServletInput;
 
+@SuppressWarnings("serial")
 @WebServlet("/edit-diagram-save")
 public class EditDiagramSaveServlet extends DiagramServlet {
-	private static final long serialVersionUID = 1L;
 
 	@Override
 	protected void do4Frame(ServletInput input) throws Exception {
@@ -15,7 +15,6 @@ public class EditDiagramSaveServlet extends DiagramServlet {
 				BlockParameter.DiagramKey.getString(input),
 				BlockParameter.Name.getString(input),
 				BlockParameter.Background.getString(input));
-
-		printCanvasFrame(input, null);
+		printCanvasFrame(input);
 	}
 }
