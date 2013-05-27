8fe2cfe05fe2e6b72da7df6f025489109fdc590d
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-27 16:26:41 +0300

    Update main.js

--- wdbuilder/src/main/webapp/js/main.js
+++ wdbuilder/src/main/webapp/js/main.js
@@ -52,10 +52,13 @@ function setCaret( diagramKey, blockKey, left, top, width, height ) {
 	c.height(height);
 	c.offset(o);
 	
+	var deleteCall = "deleteBlock('" + diagramKey + "','" + blockKey + "');" +
+		"return false;";
+	
 	// Assign some buttons for blocks:
 	var str = '<div class="btn-group btn-mini cursor-icons">';
 	str += '<a href="#" title="Edit"><i class="icon-white icon-edit"></i></a>';	
-	str += '<a href="#" title="Delete"><i class="icon-white icon-remove"></i></a>';
+	str += '<a href="#" title="Delete" onclick="' + deleteCall + '"><i class="icon-white icon-remove"></i></a>';
 	str += '&nbsp;&nbsp;</div>';
 	
 	c.html( str );
