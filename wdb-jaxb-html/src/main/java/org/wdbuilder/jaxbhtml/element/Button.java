16ae94939b82fa115eb76e40257419865ed69226
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-28 20:22:33 +0300

    Create Button.java

---
+++ wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Button.java
@@ -0,0 +1,10 @@
+package org.wdbuilder.jaxbhtml.element;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.wdbuilder.jaxbhtml.HtmlElement;
+
+@XmlRootElement(name = "button")
+public class Button extends HtmlElement {
+  
+}
