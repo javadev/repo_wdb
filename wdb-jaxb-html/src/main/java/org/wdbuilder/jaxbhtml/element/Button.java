2d61e74094d49a92a460c6a644d823acf1cf2a45
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-28 20:29:08 +0300

    Update Button.java

--- wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Button.java
+++ wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Button.java
@@ -2,9 +2,14 @@ package org.wdbuilder.jaxbhtml.element;
 
 import javax.xml.bind.annotation.XmlRootElement;
 
-import org.wdbuilder.jaxbhtml.HtmlElement;
+import org.wdbuilder.jaxbhtml.HtmlContainer;
 
 @XmlRootElement(name = "button")
-public class Button extends HtmlElement {
-  
+public class Button extends HtmlContainer {
+  public Button() {
+		super();
+	}
+	public Button( String className ) {
+		super( className );
+	}
 }
