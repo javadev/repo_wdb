dff711abb1d167cb8e608ad09024111aa660ad28
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-28 20:17:47 +0300

    Update SectionHeader.java

--- wdbuilder/src/main/java/org/wdbuilder/serialize/html/SectionHeader.java
+++ wdbuilder/src/main/java/org/wdbuilder/serialize/html/SectionHeader.java
@@ -9,31 +9,23 @@ import org.wdbuilder.gui.IUIActionClick;
 import org.wdbuilder.jaxbhtml.element.A;
 import org.wdbuilder.jaxbhtml.element.Div;
 import org.wdbuilder.jaxbhtml.element.I;
-import org.wdbuilder.jaxbhtml.element.Table;
-import org.wdbuilder.jaxbhtml.element.Td;
-import org.wdbuilder.jaxbhtml.element.Tr;
 
 public class SectionHeader extends Div {
 
-	private static final String CLASS = "header";
-	private static final String CLASS_ICON = "header-icon";
 	private static final String STYLE_FULL_WIDTH = "width:100%";
 
 	private final String title;
 
 	public SectionHeader(String title) throws JAXBException {
 		this.title = title;
-		Tr tr = new Tr();
-		tr.add(createTdTitle());
+		
+		Div div = new Div( "btn-group" );
+		// TODO: how to add the title
+		
 		for (final IUIAction icon : getIcons()) {
-			tr.add(createTdIcon(icon));
+			div.add(createLink(icon));
 		}
-		Table table = new Table(CLASS);
-		table.setStyle(STYLE_FULL_WIDTH);
-		table.add(tr);
-
-		setClassName(CLASS);
-		add(table);
+		add(div);
 	}
 
 	// May be overridden
@@ -41,19 +33,11 @@ public class SectionHeader extends Div {
 		return Collections.emptyList();
 	}
 
-	private Td createTdTitle() {
-		Td result = new Td();
-		result.setStyle(STYLE_FULL_WIDTH);
-		result.setText(title);
-		return result;
-	}
-
-	private static Td createTdIcon(IUIAction uiAction) {
+	private static A createLink(IUIAction uiAction) {
 		I i = new I();
 		i.setClassName( "icon-white " + uiAction.getResourceId() );
 		
-		Td result = new Td(CLASS_ICON);
-		A a = new A();
+		A a = new A("btn btn-mini");
 		a.setTitle( uiAction.getTitle() );
 		a.add(i);
 		if (IUIActionClick.class.isInstance(uiAction)) {
@@ -62,8 +46,7 @@ public class SectionHeader extends Div {
 		} else if (IUIActionURL.class.isInstance(uiAction)) {
 			a.setHref(IUIActionURL.class.cast(uiAction).getURL());
 		}
-		result.add(a);
-		return result;
+		return a;
 	}
 
 	public static String getImageURL(IUIAction uiAction) {
