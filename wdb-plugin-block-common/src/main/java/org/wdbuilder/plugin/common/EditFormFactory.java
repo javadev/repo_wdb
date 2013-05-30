995508a01c4fa0463893fab3c75ab5639449d6e5
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-30 18:46:19 +0300

    Update EditFormFactory.java

--- wdb-plugin-block-common/src/main/java/org/wdbuilder/plugin/common/EditFormFactory.java
+++ wdb-plugin-block-common/src/main/java/org/wdbuilder/plugin/common/EditFormFactory.java
@@ -35,7 +35,7 @@ class EditFormFactory extends UIExistingEntityFormFactory<Block> {
 		final PredefinedSelect<CommonBlock.Background> selectField = new PredefinedSelect<CommonBlock.Background>(
 				CommonBlock.Background.values(), commonBlock.getBackground() );
 
-		final TwoColumnForm form = new TwoColumnForm("edit-common-block-save")
+		final TwoColumnForm form = new TwoColumnForm("edit-common-block-save", "Edit Common Block")
 				.addHiddenField(BlockParameter.DiagramKey, diagramKey)
 				.addReadOnlyField(BlockParameter.BlockKey, entity.getKey())
 				.addTextField(BlockParameter.Name, entity.getName())
