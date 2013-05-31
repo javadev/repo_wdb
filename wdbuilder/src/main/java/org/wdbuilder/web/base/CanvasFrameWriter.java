b2e6f049f97914f1ed46e73ccaf9b2b1f9a744a3
OleksandrPavloshchuk <ap730518@gmail.com>
2013-05-31 14:12:44 +0300

    Update CanvasFrameWriter.java

--- wdbuilder/src/main/java/org/wdbuilder/web/base/CanvasFrameWriter.java
+++ wdbuilder/src/main/java/org/wdbuilder/web/base/CanvasFrameWriter.java
@@ -24,6 +24,9 @@ import org.wdbuilder.jaxbhtml.HtmlWriter;
 import org.wdbuilder.jaxbhtml.element.Area;
 import org.wdbuilder.jaxbhtml.element.Img;
 import org.wdbuilder.jaxbhtml.element.Map;
+import org.wdbuilder.jaxbhtml.element.Table;
+import org.wdbuilder.jaxbhtml.element.Td;
+import org.wdbuilder.jaxbhtml.element.Tr;
 import org.wdbuilder.plugin.IBlockPluginFacade;
 import org.wdbuilder.serialize.html.SectionHeader;
 import org.wdbuilder.serialize.html.IUIActionURL;
@@ -60,15 +63,34 @@ public class CanvasFrameWriter {
 		
 		Img img = new FrameServlet.Image(diagram, null, "frameImage", ID_IMAGE_MAP);
 		img.setOnMouseOver( "hideCaret()" );
+		
+		Tr tr = new Tr();
 
-		writer.write(img);
+		Td td = new Td();
+		td.add( img );
 
 		final ImageMap map = state.isBlockMode() ? new ImageMapForBlock(state)
 				: new ImageMapForLine(state);
 
-		writer.write(map);
+		td.add( map );
+		tr.add(td);
 		
-		SectionHeader header = new SectionHeader() {
+		SectionHeader header = createToolbar(state, diagram, diagramKey);
+		Td headerTd = new Td();
+		headerTd.setStyle("vertical-align:top");
+		headerTd.add(header);
+		tr.add(headerTd);
+		
+		Table table = new Table();
+		table.add( tr );
+		
+		writer.write( table );		
+	}
+
+	private SectionHeader createToolbar(final ApplicationState state,
+			final Diagram diagram, final String diagramKey)
+			throws JAXBException {
+		return new SectionHeader() {
 
 			@Override
 			public Iterable<IUIAction> getIcons() {
@@ -180,7 +202,6 @@ public class CanvasFrameWriter {
 				};
 			}
 		};
-		writer.write(header);		
 	}
 
 	private abstract class ImageMap extends Map {
