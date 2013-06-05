84763631c86b6467f8697eab208b9c0fb82d9ccf
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-05 13:20:15 +0300

    Create Label.java

---
+++ wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Label.java
@@ -0,0 +1,8 @@
+package org.wdbuilder.jaxbhtml.element;
+
+import javax.xml.bind.annotation.XmlRootElement;
+import org.wdbuilder.jaxbhtml.HtmlContainer;
+
+@XmlRootElement(name = "label")
+public class Label extends HtmlContainer {
+}
