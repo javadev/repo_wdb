67b6ef79ba559aac8952d27c46de8c9fbf14cc2e
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-07 15:29:25 +0300

    Update CreateFormFactory.java

--- wdb-plugin-block-icon/src/main/java/org/wdbuilder/plugin/icon/CreateFormFactory.java
+++ wdb-plugin-block-icon/src/main/java/org/wdbuilder/plugin/icon/CreateFormFactory.java
@@ -1,13 +1,9 @@
 package org.wdbuilder.plugin.icon;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.wdbuilder.gui.PredefinedSelect;
 import org.wdbuilder.gui.TwoColumnForm;
 import org.wdbuilder.gui.UINewBlockFormFactory;
 import org.wdbuilder.input.BlockParameter;
-import org.wdbuilder.input.IParameter;
 import org.wdbuilder.plugin.icon.IconBlockPluginFacade.Parameter;
 import org.wdbuilder.plugin.icon.domain.IconBlock;
 
@@ -32,24 +28,12 @@ class CreateFormFactory extends UINewBlockFormFactory {
 
 	@Override
 	public String getSubmitCall() {
-		StringBuilder sb = new StringBuilder(128).append("submitCreateBlock('")
-				.append(diagramKey).append("','")
-				.append(blockClass.getCanonicalName()).append("',");
-		appendFieldNames(sb, getParameters());
-		sb.append(')');
-		return sb.toString();
+		return "submitCreateBlock()";
 	}
 
 	@Override
 	public String getTitle() {
 		return "Create Icon Block";
-	}
-
-	private static Iterable<IParameter> getParameters() {
-		List<IParameter> result = new ArrayList<IParameter>(2);
-		result.add(BlockParameter.Name);
-		result.add(Parameter.IconID);
-		return result;
 	}
 
 }
