43082203918ffe6afc7d7763d48c8e125bf63dc1
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-07 17:58:17 +0300

    Update common.js

--- wdbuilder/src/main/webapp/js/common.js
+++ wdbuilder/src/main/webapp/js/common.js
@@ -1,63 +1,28 @@
 
-// Generalized XMLHttpRequest creation (from Javascript.RU)
-function getXmlHttp() {
-	var xmlhttp;
-	try {
-		xmlhttp = new ActiveXObject("Msxml2.XMLHTTP");
-	} catch (e) {
-		try {
-			xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-		} catch (E) {
-			xmlhttp = false;
-		}
-	}
-	if (!xmlhttp && 'undefined' != typeof XMLHttpRequest) {
-		xmlhttp = new XMLHttpRequest();
-	}
-
-	return xmlhttp;
-}
-
-// Load HTML context using XMLHttpRequest (GET method) (from Javascript.RU) 
-function loadByAJAX(url, acceptor) {
-	document.body.style.cursor = "wait";
-	var request = getXmlHttp();
-	if (request) {
-		request.onreadystatechange = function() {
-			if (4 == request.readyState) {
-				document.body.style.cursor = "default";
-				if (200 == request.status) {
-					clearError();
-					acceptor( request.responseText);
-					
-				} else {
-					// Some error:
-					showError(request.responseText);
-				}
-			}
-		};
-		// Request without caching
-		request.open("GET", url, true);
-		request.setRequestHeader("connection", "close");
-		request.send(null);
-	}
-}
-
 // Request some HTML content by XMLHttpRequest and show it in some screen element 
-function loadContent(url, containerId, callback ) {
-	loadByAJAX(url, function(response) {
-		var elem = document.getElementById(containerId);
-		if (!elem) {
-			return;
-		}
-		elem.innerHTML = response;
-		var len = response.length;
-		elem.style.display = ( 0==len ) ? "none" : "block";
-		document.body.style.cursor = "default";
-		if( callback ) {
-			callback();
-		}
-	});
+function loadContent(aUrl, aContainerId, callback ) {
+	$.ajax({
+		type: "GET",
+		dataType: "html",
+		url: aUrl,
+		success : function( data ) {
+			document.body.style.cursor = "default";
+			var len = data.length;
+			if( 0!=len ) {
+				$( '#' + aContainerId ).show().html( data );
+				if( callback ) {
+					callback();
+				}
+				clearError();
+			} else {
+				$( '#' + aContainerId ).hide();
+			}
+		},
+		error : function ( data ) {
+			document.body.style.cursor = "default";
+			showError(data);
+		}		
+	});	
 }
 
 function submitForm( aUrl, anAcceptor) {
@@ -74,7 +39,7 @@ function submitForm( aUrl, anAcceptor) {
 		},
 		error : function ( data ) {
 			document.body.style.cursor = "default";
-			showError(request.responseText);
+			showError(data);
 		}		
 	});
 }
