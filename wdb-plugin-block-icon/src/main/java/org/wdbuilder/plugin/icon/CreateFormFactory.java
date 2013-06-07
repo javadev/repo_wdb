df76a8425ec870c630e3a75cd0af456123905257
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-07 14:00:12 +0300

    Update CreateFormFactory.java

--- wdb-plugin-block-icon/src/main/java/org/wdbuilder/plugin/icon/CreateFormFactory.java
+++ wdb-plugin-block-icon/src/main/java/org/wdbuilder/plugin/icon/CreateFormFactory.java
@@ -9,6 +9,7 @@ import org.wdbuilder.gui.UINewBlockFormFactory;
 import org.wdbuilder.input.BlockParameter;
 import org.wdbuilder.input.IParameter;
 import org.wdbuilder.plugin.icon.IconBlockPluginFacade.Parameter;
+import org.wdbuilder.plugin.icon.domain.IconBlock;
 
 class CreateFormFactory extends UINewBlockFormFactory {
 
@@ -23,6 +24,7 @@ class CreateFormFactory extends UINewBlockFormFactory {
 				Icon.values(), Icon.Avatar);
 		final TwoColumnForm form = new TwoColumnForm("create-icon-block-save", "Create Icon Block")
 				.addHiddenField(BlockParameter.DiagramKey, diagramKey)
+				.addHiddenField(BlockParameter.BlockClass, IconBlock.class.getCanonicalName())
 				.addTextField(BlockParameter.Name, "")
 				.addSelectField(Parameter.IconID, iconSelectField);
 		return form;
