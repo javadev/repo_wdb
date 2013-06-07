2188e197ac2cf345a60945debd81fd0f0ee57d37
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-07 15:21:58 +0300

    Update EditFormFactory.java

--- wdb-plugin-link-default/src/main/java/org/wdbuilder/plugin/defaultlink/EditFormFactory.java
+++ wdb-plugin-link-default/src/main/java/org/wdbuilder/plugin/defaultlink/EditFormFactory.java
@@ -1,14 +1,10 @@
 package org.wdbuilder.plugin.defaultlink;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.wdbuilder.domain.Link;
 import org.wdbuilder.gui.PredefinedSelect;
 import org.wdbuilder.gui.TwoColumnForm;
 import org.wdbuilder.gui.UIExistingEntityFormFactory;
 import org.wdbuilder.input.BlockParameter;
-import org.wdbuilder.input.IParameter;
 import org.wdbuilder.plugin.defaultlink.DefaultLinkPluginFacade.Parameter;
 import org.wdbuilder.view.line.LineStyle;
 import org.wdbuilder.view.line.end.LineEnd;
@@ -49,10 +45,7 @@ class EditFormFactory extends UIExistingEntityFormFactory<Link> {
 	@Override
 	public String getSubmitCall() {
 		StringBuilder sb = new StringBuilder(128).append("submitEditLink('")
-				.append(diagramKey).append("','").append(entity.getKey())
-				.append("',");
-		appendFieldNames(sb, getParameters());
-		sb.append(')');
+				.append(diagramKey).append("')");
 		return sb.toString();
 	}
 
@@ -60,15 +53,4 @@ class EditFormFactory extends UIExistingEntityFormFactory<Link> {
 	public String getTitle() {
 		return "Edit Link";
 	}
-
-	private static Iterable<IParameter> getParameters() {
-		List<IParameter> result = new ArrayList<IParameter>(2);
-		result.add(BlockParameter.Name);
-		result.add(Parameter.LineColor);
-		result.add(Parameter.LineStyle);
-		result.add(Parameter.StartType);
-		result.add(Parameter.EndType);
-		return result;
-	}
-
 }
