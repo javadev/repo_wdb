cbfc3e682d5b449d9cb887e8ebba80587fde38eb
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-10 13:02:13 +0300

    Update IServiceFacade.java

--- wdb-api/src/main/java/org/wdbuilder/service/IServiceFacade.java
+++ wdb-api/src/main/java/org/wdbuilder/service/IServiceFacade.java
@@ -4,12 +4,14 @@ import org.wdbuilder.domain.Block;
 import org.wdbuilder.domain.Link;
 import org.wdbuilder.plugin.IBlockPluginFacade;
 import org.wdbuilder.plugin.ILinkPluginFacade;
+import org.wdbuilder.plugin.ILinkRenderContext;
+import org.wdbuilder.plugin.IRenderContext;
 
 public interface IServiceFacade {
 
 	DiagramService getDiagramService();
 
-	IPluginFacadeRepository<Block, IBlockPluginFacade> getBlockPluginRepository();
+	IPluginFacadeRepository<Block, IBlockPluginFacade, IRenderContext> getBlockPluginRepository();
 
-	IPluginFacadeRepository<Link, ILinkPluginFacade> getLinkPluginRepository();
+	IPluginFacadeRepository<Link, ILinkPluginFacade, ILinkRenderContext> getLinkPluginRepository();
 }
