e96c14aa6364408b652628d478a07ead87457d23
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-10 12:48:04 +0300

    Create IEntityPluginFacade.java

---
+++ wdb-api/src/main/java/org/wdbuilder/plugin/IEntityPluginFacade.java
@@ -0,0 +1,19 @@
+package org.wdbuilder.plugin;
+
+import org.wdbuilder.domain.Entity;
+import org.wdbuilder.gui.UIExistingEntityFormFactory;
+import org.wdbuilder.input.InputAdapter;
+import org.wdbuilder.validator.IValidator;
+
+public interface IEntityPluginFacade<T extends Entity> {
+
+  public Class<?> getEntityClass();
+
+	public IValidator<T> getValidator();
+
+	public UIExistingEntityFormFactory<T> getEditFormFactory(String diagramKey,
+			T entity);
+	
+	public T create(InputAdapter input);
+
+}
