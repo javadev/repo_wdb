4f018e09aa319717ee51ab3ad8b1f8de700de875
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-10 12:16:46 +0300

    Update Input.java

--- wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Input.java
+++ wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/element/Input.java
@@ -5,39 +5,51 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 import org.wdbuilder.jaxbhtml.HtmlElement;
 
-
-@XmlRootElement(name="input")
+@XmlRootElement(name = "input")
 public class Input extends HtmlElement {
-    @XmlAttribute
+
 	private String value;
-	@XmlAttribute
 	private String name;
-	@XmlAttribute
 	private String type;
-	
+
 	private Input() {
 		super();
 	}
-	
-	public void setName( String name ) {
+
+	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public void setValue( String value ) {
+
+	public void setValue(String value) {
 		this.value = value;
 	}
-	
-	protected void setType( String type ) {
+
+	protected void setType(String type) {
 		this.type = type;
 	}
-	
+
+	@XmlAttribute
+	public String getName() {
+		return this.name;
+	}
+
+	@XmlAttribute
+	public String getValue() {
+		return this.value;
+	}
+
+	@XmlAttribute
+	public String getType() {
+		return this.type;
+	}
+
 	public static class Text extends Input {
 		public Text() {
 			super();
 			setType("text");
 		}
 	}
-	
+
 	public static class Hidden extends Input {
 		public Hidden() {
 			super();
