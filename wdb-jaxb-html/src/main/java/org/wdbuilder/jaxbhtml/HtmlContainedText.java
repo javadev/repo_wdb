5b2864b33fc6797f8e375a564106578f556ec7a1
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-10 12:14:48 +0300

    Update HtmlContainedText.java

--- wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/HtmlContainedText.java
+++ wdb-jaxb-html/src/main/java/org/wdbuilder/jaxbhtml/HtmlContainedText.java
@@ -3,14 +3,19 @@ package org.wdbuilder.jaxbhtml;
 import javax.xml.bind.annotation.XmlValue;
 
 public class HtmlContainedText implements IHtml {
-    @XmlValue
+
 	private String text;
-	
+
 	public HtmlContainedText() {
-		
-	}	
-	
-	public HtmlContainedText( String text ) {
+
+	}
+
+	public HtmlContainedText(String text) {
 		this.text = text;
+	}
+
+	@XmlValue
+	public String getText() {
+		return this.text;
 	}
 }
