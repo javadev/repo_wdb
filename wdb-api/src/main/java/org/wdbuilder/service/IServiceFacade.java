60db9b9e62b4b70bf87c063c015492978ec7ee16
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-10 12:51:58 +0300

    Update IServiceFacade.java

--- wdb-api/src/main/java/org/wdbuilder/service/IServiceFacade.java
+++ wdb-api/src/main/java/org/wdbuilder/service/IServiceFacade.java
@@ -5,7 +5,6 @@ import org.wdbuilder.domain.Link;
 import org.wdbuilder.plugin.IBlockPluginFacade;
 import org.wdbuilder.plugin.ILinkPluginFacade;
 
-// TODO "the root object" for diagram service and repository of plugins (2013/05/06)
 public interface IServiceFacade {
 
 	DiagramService getDiagramService();
