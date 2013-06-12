9fd17085de8564e51b6bb54c6b6c9b8bd322f3b6
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-12 15:06:11 +0300

    Create ImportDiagramServlet.java

---
+++ wdbuilder/src/main/java/org/wdbuilder/web/ImportDiagramServlet.java
@@ -0,0 +1,31 @@
+package org.wdbuilder.web;
+
+import java.io.PrintWriter;
+
+import javax.servlet.annotation.WebServlet;
+
+import org.wdbuilder.gui.TwoColumnForm;
+import org.wdbuilder.input.BlockParameter;
+import org.wdbuilder.jaxbhtml.HtmlWriter;
+import org.wdbuilder.web.base.DiagramServiceServlet;
+import org.wdbuilder.web.base.ServletInput;
+
+@SuppressWarnings("serial")
+@WebServlet("/import-diagram")
+public class ImportDiagramServlet extends DiagramServiceServlet {
+
+  @Override
+	protected void do4DiagramService(ServletInput input) throws Exception {
+		final PrintWriter writer = input.getResponse().getWriter();
+
+		final TwoColumnForm form = new TwoColumnForm("import-diagram-save",
+				"Import Diagram").addFileField(BlockParameter.DiagramKey)
+				.addFooter("submitImportDiagram()", "refreshDiagramList()");
+		new HtmlWriter(writer).write(form);
+	}
+
+	@Override
+	protected String getContentType() {
+		return DiagramServlet.CONTENT_TYPE_XML;
+	}
+}
