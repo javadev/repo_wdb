d12535dd0743a2b0126a0c1d65c7201d006e6407
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-13 18:02:53 +0300

    Update IUIActionClick.java

--- wdb-api/src/main/java/org/wdbuilder/gui/IUIActionClick.java
+++ wdb-api/src/main/java/org/wdbuilder/gui/IUIActionClick.java
@@ -10,4 +10,9 @@ public abstract class IUIActionClick implements IUIAction {
 	public void setActionToHTMLElement(HtmlElement element) {
 		element.setOnClick(getOnClickHandler());
 	}
+	
+	@Override
+	public String getClassName() {
+		return "";
+	}	
 }
