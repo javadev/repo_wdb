0b7e4721c7c399e2cbb6a683d80bc918b67a9947
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-14 15:59:04 +0300

    Update UIEntityFormFactory.java

--- wdb-api/src/main/java/org/wdbuilder/gui/UIEntityFormFactory.java
+++ wdb-api/src/main/java/org/wdbuilder/gui/UIEntityFormFactory.java
@@ -1,7 +1,5 @@
 package org.wdbuilder.gui;
 
-import org.wdbuilder.input.IParameter;
-
 abstract class UIEntityFormFactory {
 
 	public abstract TwoColumnForm getForm();
@@ -15,15 +13,4 @@ abstract class UIEntityFormFactory {
 	UIEntityFormFactory(String diagramKey) {
 		this.diagramKey = diagramKey;
 	}
-
-	protected void appendFieldNames(StringBuilder sb,
-			Iterable<IParameter> params) {
-		sb.append('[');
-		for (final IParameter param : params) {
-			sb.append("'").append(param.getName()).append("',");
-		}
-		int len = sb.length();
-		sb.replace(len - 1, len, "]");
-	}
-
 }
