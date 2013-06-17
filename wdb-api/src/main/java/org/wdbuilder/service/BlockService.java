6671e239dfb0b99cdef95b12cd9d96aa81100872
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-17 12:24:12 +0300

    Update BlockService.java

--- wdb-api/src/main/java/org/wdbuilder/service/BlockService.java
+++ wdb-api/src/main/java/org/wdbuilder/service/BlockService.java
@@ -2,13 +2,5 @@ package org.wdbuilder.service;
 
 import org.wdbuilder.domain.Block;
 
-public interface BlockService {
-	
-	void setLocation(String blockKey, int offsetX, int offsetY);
-
-	void delete(String blockKey);
-
-	String persist(Block block);
-
-	void update(String blockKey, Block block);	
+public interface BlockService extends EntityServiceBase<Block> {	
 }
