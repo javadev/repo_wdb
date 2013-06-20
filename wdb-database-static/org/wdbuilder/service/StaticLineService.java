928d2db50f605f6388d61a28ca7735b87c572512
OleksandrPavloshchuk <ap730518@gmail.com>
2013-06-20 15:20:04 +0300

    Update StaticLineService.java

--- wdb-database-static/org/wdbuilder/service/StaticLineService.java
+++ wdb-database-static/org/wdbuilder/service/StaticLineService.java
@@ -8,8 +8,8 @@ import org.wdbuilder.domain.helper.Point;
 import org.wdbuilder.plugin.ILinkPluginFacade;
 
 class StaticLinkService extends StaticDiagramRelatedService implements
-  	EntityServiceBase<Link> {
-
+		EntityServiceBase<Link> {
+	
 	StaticLinkService(Diagram diagram, IServiceFacade serviceFacade) {
 		super(diagram, serviceFacade);
 	}
@@ -19,7 +19,7 @@ class StaticLinkService extends StaticDiagramRelatedService implements
 		if (linkKey.isEmpty()) {
 			return;
 		}
-		diagramHelper.removeLinkByKey(linkKey);
+		removeByKey(linkKey, diagram.getLinks() );
 	}
 
 	@Override
@@ -27,8 +27,8 @@ class StaticLinkService extends StaticDiagramRelatedService implements
 		final String key = UUID.randomUUID().toString();
 		link.setKey(key);
 		
-		if (!diagramHelper.hasLinkWithSameEnds(link)) {
-			diagramHelper.getDiagram().getLinks().add(link);
+		if (!hasLinkWithSameEnds(link)) {
+			diagram.getLinks().add(link);
 		}	
 		
 		return key;
@@ -37,7 +37,7 @@ class StaticLinkService extends StaticDiagramRelatedService implements
 
 	@Override
 	public void setPosition(String linkKey, int x, int y) {
-		final Link link = diagramHelper.findLinkByKey(linkKey);
+		final Link link = findByKey(linkKey, diagram.getLinks() );
 		if (null == link) {
 			return;
 		}
@@ -46,8 +46,7 @@ class StaticLinkService extends StaticDiagramRelatedService implements
 
 	@Override
 	public void update(String linkKey, Link link) {
-		final Diagram diagram = diagramHelper.getDiagram();
-		final Link savedLink = diagramHelper.findLinkByKey(linkKey);
+		final Link savedLink = findByKey(linkKey,  diagram.getLinks() );
 		if (null == savedLink) {
 			// Nothing to update
 			return;
@@ -75,6 +74,17 @@ class StaticLinkService extends StaticDiagramRelatedService implements
 	private ILinkPluginFacade getLinkPluginFacade(final Link link) {
 		return serviceFacade.getLinkPluginRepository().getFacade(
 				link.getClass());
+	}	
+	
+	private boolean hasLinkWithSameEnds(Link probeLink) {
+		for (Link link : diagram.getLinks()) {
+			if (link.getSockets().get(0).equals(probeLink.getSockets().get(0))
+					&& link.getSockets().get(1)
+							.equals(probeLink.getSockets().get(1))) {
+				return true;
+			}
+		}
+		return false;
 	}	
 
 }
